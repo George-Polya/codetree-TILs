a8a086a842f543762c14bc27207b3bbc7d36ecb6
Kim Hee Su <wlskaka4@gmail.com>
2023-11-18 15:58:34 +0900

    Title: 가장 긴 좌우 대칭인 문자열 찾기 2, Time: 140ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 231118/가장 긴 좌우 대칭인 문자열 찾기 2/longest-palindrome-2.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static String str;
+	static char inputStr[];
+	static int n;
+	static int A[];
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		str = br.readLine();
+		int len = str.length();
+		
+		n = 2 * len + 1;
+		inputStr = new char[n];
+		A = new int[n];
+		for(int i = 0; i < len; i++) {
+			inputStr[i * 2] = '#';
+			inputStr[i * 2 + 1] = str.charAt(i);
+		}
+		
+		inputStr[n-1] = '#';
+//		System.out.println(Arrays.toString(inputStr));
+		
+		int r = -1;
+		int p = -1;
+		
+		for(int i = 0; i <n ;i++) {
+			if(r < i)
+				A[i] = 0;
+			else {
+				int ii = 2 * p - i;
+				A[i] = Math.min(r - i , A[ii]);
+			}
+			
+			while(i - A[i] - 1 >=0 && i + A[i] + 1 < n 
+					&& inputStr[i - A[i] -1] == inputStr[i + A[i] + 1])
+				A[i]++;
+			
+			
+			if(i + A[i] > r) {
+				r = i + A[i];
+				p = i;
+			}
+		}
+		
+		int ans = 0;
+		for(int i = 0; i < n ;i++)
+			ans = Math.max(ans, 2 * A[i] + 1);
+		System.out.println(ans/2);
+		
+	}
+}
