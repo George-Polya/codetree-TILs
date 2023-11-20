7410a91d9f13b4b869a4b86604809d087505feae
Kim Hee Su <wlskaka4@gmail.com>
2023-11-20 21:09:14 +0900

    Title: 가장 많이 등장한 부분 문자열, Time: 223ms, Memory: 24MB, Status: Passed - CodeTree

--- 231120/가장 많이 등장한 부분 문자열/most-common-substring.java
+++ 231120/가장 많이 등장한 부분 문자열/most-common-substring.java
@@ -14,6 +14,10 @@ public class Main {
 	static int mod[] = new int[] {(int)1e9+7, (int)1e9+9};
 	
 	static StringTokenizer st;
+	
+	static long getUniqueKey(long h1, long h2) {
+		return h1 * Math.max(mod[0], mod[1]) + h2;
+	}
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		st = new StringTokenizer(br.readLine());
@@ -23,10 +27,8 @@ public class Main {
 		
 		pPow = new long[2][n+1];
 		
-		Map<Long, Integer> maps[] = new HashMap[2];
 		for(int k = 0; k < 2; k++) {
 			pPow[k][0] = 1;
-			maps[k] = new HashMap<>();
 			for(int i = 1; i<=n;i++) {
 				pPow[k][i] = (pPow[k][i-1] * p[k]) % mod[k];
 			}
@@ -40,31 +42,29 @@ public class Main {
 			}
 		}
 		
-		
-		maps[0].put(tH[0], 1);
-		
-		maps[1].put(tH[1], 1);
+		int ans = 0;
+		HashMap<Long, Integer> map = new HashMap<>();
+		long key = getUniqueKey(tH[0], tH[1]);
+		map.put(key, map.getOrDefault(key, 0) + 1);
+		ans = Math.max(ans, map.get(key));
 		
 		
 		for(int i = 1; i <= n - m; i++) {
 			for(int k = 0; k < 2; k++) {
-				tH[k] = (tH[k] * p[k] - toInt(text.charAt(i-1)) * pPow[k][m]) % mod[k];
-				
-				maps[k].put(tH[k], maps[k].getOrDefault(tH[k], 1) + 1);
+				tH[k] = (tH[k] * p[k] - toInt(text.charAt(i-1)) * pPow[k][m] + toInt(text.charAt(i + m - 1))) % mod[k];
 				
 				
 				if(tH[k] < 0)
 					tH[k] += mod[k];
 			}
-		}
-		
-		
-		int ans = 0;
-		for(long key : maps[0].keySet()) {
-			ans = Math.max(ans, maps[0].get(key));
+			
+			key = getUniqueKey(tH[0], tH[1]);
+			map.put(key, map.getOrDefault(key, 0) + 1);
+			ans = Math.max(ans, map.get(key));
 		}
 		
 		System.out.println(ans);
+		
 	}
 	
 }
