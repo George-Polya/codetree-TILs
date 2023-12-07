ad1b669799ba63052bae09142b9849b4fc08af54
Kim Hee Su <wlskaka4@gmail.com>
2023-12-07 11:42:01 +0900

    Title: 가장 긴 좌우 대칭인 문자열 찾기 4, Time: 342ms, Memory: 36MB, Status: Passed - CodeTree

--- 231207/가장 긴 좌우 대칭인 문자열 찾기 4/longest-palindrome-4.java
+++ 231207/가장 긴 좌우 대칭인 문자열 찾기 4/longest-palindrome-4.java
@@ -5,56 +5,38 @@ import java.io.*;
 public class Main {
 	static int n;
 	static String text;
-	static int dp[][];
-	static int length[][];
-	static int symmetry(int start, int end) {
-		int len = end - start + 1;
-		int radius = len / 2;
-		
-		for(int i = 0; i < radius; i++) {
-			if(text.charAt(start + i) != text.charAt(end - i))
-				return 0;
-		}
-		return len;
-	}
-	
-	static int findMax(int i, int j) {
-		if(dp[i][j] != -1)
-			return dp[i][j];
-		
-		if(i == j) {
-			return dp[i][j] = 1;
-		}
-		
-		int best = -1;
-		for(int k = i; k<j;k++) {
-			int len = Math.max(length[i][j], Math.max(findMax(i,k), findMax(k+1,j)));
-			best = Math.max(best,len);
-		}
-		
-		return dp[i][j] = best;
-	}
+	static boolean dp[][];
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		text = br.readLine();
 		n = text.length();
 		text = "#" + text;
-		length = new int[n+1][n+1];
+		dp = new boolean[n+1][n+1];
+		for(int i = 1; i<=n;i++) {
+			dp[i][i] = true;
+		}
 		
-		for(int i = 1; i<=n; i++) {
-			length[i][i] = 1;
-			for(int j = i + 1; j<=n; j++) {
-				length[i][j] = symmetry(i,j);
+		for(int i = 1; i<n;i++) {
+			dp[i][i+1] = (text.charAt(i) == text.charAt(i+1));
+		}
+		
+		int ans = 0;
+		for(int len = 3; len <= n; len++) {
+			for(int i = 1; i<=n; i++) {
+				int j = i + len - 1;
+				if(j > n)
+					continue;
+				
+				if(dp[i+1][j-1] && text.charAt(i) == text.charAt(j)) {
+					dp[i][j] = true;
+					ans = len;
+				}
+				
 			}
 		}
-		dp = new int[n+1][n+1];
-		for(int i = 1; i<=n; i++) {
-			for(int j = 1; j<=n; j++)
-				dp[i][j] = -1;
-		}
 		
-		System.out.println(findMax(1,n));
+		System.out.println(ans);
 		
 		
 	}
