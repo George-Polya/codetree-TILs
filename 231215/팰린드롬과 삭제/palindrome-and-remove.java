6d6487fbfed0bda3d50c865eb6be8dbfa1a8c5d7
Kim Hee Su <wlskaka4@gmail.com>
2023-12-15 09:45:44 +0900

    Title: 팰린드롬과 삭제, Time: 187ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 231215/팰린드롬과 삭제/palindrome-and-remove.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+	static String str;
+	static int n;
+	static int dp[][];
+	static int INT_MAX = (int)1e9;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		str = br.readLine();
+		n = str.length();
+		dp = new int[n][n];
+		for(int i = 0; i < n ;i++) {
+			Arrays.fill(dp[i], INT_MAX);
+		}
+		
+		for(int i = 0; i< n; i++) {
+			dp[i][i] = 0;
+			if(i + 1 != n) {
+				dp[i][i+1] = (str.charAt(i) == str.charAt(i+1)) ? 0 : 1;
+			}
+		}
+		
+		
+		for(int len = 3; len <=n; len++) {
+			for(int i = 0; i < n;i++) {
+				int j = i + len - 1;
+				if(j >= n)
+					continue;
+				dp[i][j] = Math.min(dp[i][j], dp[i+1][j] + 1);
+				dp[i][j] = Math.min(dp[i][j], dp[i][j-1] + 1);
+				
+				if(str.charAt(i) == str.charAt(j)) {
+					dp[i][j] = Math.min(dp[i][j], dp[i+1][j-1]);
+				}
+			}
+		}
+		System.out.println(dp[0][n-1]);
+	}
+}
