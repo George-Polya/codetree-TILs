085e886f22242c4fae0e33efef090c0ae5779d8a
Kim Hee Su <wlskaka4@gmail.com>
2023-12-16 16:14:03 +0900

    Title: 인접한 칸 선택, Time: 85ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231216/인접한 칸 선택/pick-adjacent-cells.java
@@ -0,0 +1,78 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+	static int n,m;
+	static int board[][], dp[][];
+	static StringTokenizer st;
+	
+	static boolean isOverlap(int j, int k) {
+		if( (j & k) > 0)
+			return true;
+		for(int x = 0; x < m-1; x++) {
+			if( (((k >> x) & 1) == 1) && (((k >> (x+1) ) & 1) == 1))
+				return true;
+		}
+		return false;
+	}
+	
+	static boolean isWall(int i,int k) {
+		for(int x = 0; x< m; x++) {
+			if((((k >> x) & 1) == 1) && board[i+1][x+1] == 1)
+				return true;
+		}
+		return false;
+	}
+	
+	static int getNum(int k) {
+		int ret = 0;
+		for(int x = 0; x< m; x++) {
+			if (((k >> x) & 1) == 1)
+				ret++;
+		}
+		return ret;
+	}
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		board = new int[n+1][m+1];
+		for(int y=1; y<=n; y++) {
+			st = new StringTokenizer(br.readLine());
+			for(int x=1;x<=m;x++) {
+				board[y][x] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		dp = new int[n + 1][1 << n];
+		for(int i = 0; i <= n;i++) {
+			Arrays.fill(dp[i], -1);
+		}
+		dp[0][0] = 0;
+		
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < (1<<m);j++) {
+				if(dp[i][j] == -1)
+					continue;
+				
+				for(int k = 0; k < (1<<m);k++) {
+					if(isOverlap(j,k))
+						continue;
+					
+					if(isWall(i, k))
+						continue;
+					
+					dp[i + 1][j] = Math.max(dp[i + 1][j], dp[i][j] + getNum(k));
+				}
+			}
+		}
+		
+		int ans = -1;
+		for(int i = 0; i < (1 << m);i++) {
+			ans = Math.max(ans, dp[n][i]);
+		}
+		System.out.println(ans);
+	}
+}
