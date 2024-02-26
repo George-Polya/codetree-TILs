a770ed131c93fa22ad28b449697fe4723def7bb0
Kim Hee Su <wlskaka4@gmail.com>
2024-02-26 21:36:34 +0900

    Title: 연결리스트 2, Time: 103ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240226/연결리스트 2/linked-list2.java
@@ -0,0 +1,118 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	static int n,q;
+	static StringTokenizer st;
+	static class Node{
+		int id;
+		Node next, prev;
+		
+		public Node(int id) {
+			this.id = id;
+			this.next = this.next = null;
+		}
+	}
+	
+	static Node[] nodeList;
+	
+	static void insertPrev(int u, int v) {
+		if(nodeList[u] == null) {
+			Node node = new Node(u);
+			nodeList[u] = node;
+		}
+		
+		Node uNode = nodeList[u];
+		
+		Node singleton = new Node(v);
+		
+		singleton.next = uNode;
+		singleton.prev = uNode.prev;
+		
+		if(singleton.prev != null)
+			singleton.prev.next = singleton;
+		if(singleton.next != null)
+			singleton.next.prev = singleton;
+			
+		nodeList[v] = singleton;
+	}
+	
+	static void insertNext(int u, int v) {
+		if(nodeList[u] == null) {
+			Node node = new Node(u);
+			nodeList[u] = node;
+		}
+		
+		Node uNode = nodeList[u];
+		
+		Node singleton = new Node(v);
+		
+		singleton.prev = uNode;
+		singleton.next = uNode.next;
+		
+		if(singleton.prev != null)
+			singleton.prev.next = singleton;
+		if(singleton.next != null)
+			singleton.next.prev = singleton;
+		
+		nodeList[v] = singleton;
+	}
+	
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		q = Integer.parseInt(br.readLine());
+		nodeList = new Node[n+1];
+//		StringBuilder sb = new StringB
+		for(int i = 0; i < q;i++) {
+			st = new StringTokenizer(br.readLine());
+			int oper = Integer.parseInt(st.nextToken());
+			int u = Integer.parseInt(st.nextToken());
+			switch(oper) {
+			case 1:
+				if(nodeList[u] != null) {
+					Node node = nodeList[u];
+					if(node.prev != null)
+						node.prev.next = node.next;
+					if(node.next != null)
+						node.next.prev = node.prev;
+					nodeList[u] = null;
+				}
+				break;
+			case 2:{
+				int v = Integer.parseInt(st.nextToken());
+                insertPrev(u, v);
+			
+			}
+				break;
+			case 3:{
+				int v = Integer.parseInt(st.nextToken());
+				insertNext(u,v);
+			}
+				break;
+			case 4:
+				if(nodeList[u] != null) {
+					Node node = nodeList[u];
+					System.out.print(node.prev == null ? 0 : node.prev.id +" ");
+					System.out.print(node.next == null ? 0 : node.next.id+"\n");
+				}else{
+                    System.out.println("0 0");
+                }
+				break;
+				
+			}
+		}
+
+        for(int i = 1; i<=n; i++){
+            if(nodeList[i] == null)
+                System.out.print(0+" ");
+            else{
+                Node node = nodeList[i];
+                System.out.print(node.next == null ? 0 : node.next.id+" ");
+            }
+            
+        }
+		
+		
+	}
+}
