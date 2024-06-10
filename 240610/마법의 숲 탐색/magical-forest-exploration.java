4242ed62b4b0e162d25cfe7dee58e2e8336abffa
Kim Hee Su <wlskaka4@gmail.com>
2024-06-10 18:08:10 +0900

    Title: 마법의 숲 탐색, Time: 209ms, Memory: 21MB, Status: Passed - CodeTree

--- 240610/마법의 숲 탐색/magical-forest-exploration.java
+++ 240610/마법의 숲 탐색/magical-forest-exploration.java
@@ -1,242 +1,119 @@
-import java.io.*;
-import java.time.*;
-import java.time.format.DateTimeFormatter;
 import java.util.*;
-
+import java.io.*;
 public class Main {
-	static int r,c,k;
-	static StringTokenizer st;
-	static int board[][];
-	static int dy[] = {-1,0,1,0};
-	static int dx[] = {0,1,0,-1};
-	static boolean OOB(int y, int x) {
-		return y<=0 || y> r || x<=0 || x>c;
-	}
-	
-	static class Pair{
-		int y,x;
-		public Pair(int y,int x) {
-			this.y = y;
-			this.x = x;
-		}
-		
-		public String toString() {
-			return y+" "+x;
-		}
-	}
-	
-	static void printBoard(int board[][]) {
-		for(int y=1; y<=r; y++) {
-			for(int x=1; x<=c; x++) {
-				System.out.printf("%3d", board[y][x]);
-			}
-			System.out.println();
-		}
-	}
-	
-	static class Tuple{
-		int first,second,third;
-		public Tuple(int first,int second, int third) {
-			this.first = first;
-			this.second = second;
-			this.third = third;
-		}
-		
-		public String toString() {
-			return first+" "+second+" "+third;
-		}
-		
-		public boolean isSame(int first, int second) {
-			return this.first == first && this.second == second;
-		}
-	}
-	
-//	static class Tuple{
-//		int first,second,third;
-//		public Tuple(int y,int x, int id) {
-//			this.first = y;
-//			this.second = x;
-//			this.third = id;
-//		}
-//		
-//		public boolean isSame(int first, int second) {
-//			return this.first == first && this.second == second;
-//		}
-//	}
-	
-	static Tuple NO_POS = new Tuple(-3,-3,-3);
-	static Tuple END = new Tuple(-4,-4,-4);
-	static class Golem{
-		int id;
-		int cy,cx,exitDir; // 골렘 중앙과 출구의 방향 
-		Tuple fairy;
-		Pair exit;
-		public Golem(int id, int cy, int cx, int exitDir) {
-			this.id = id;
-			this.cy = cy;
-			this.cx = cx;
-			this.exitDir = exitDir;
-			exit = new Pair(cy+dy[exitDir], cx+dx[exitDir]);
-			fairy = new Tuple(cy,cx,id);
-		}
-		
-		private Tuple getNxtPos(int cy, int cx, int exitDir, int moveDirs[], int tempDir) {
-			int y = cy + dy[tempDir];
-			int x = cx + dx[tempDir];
-			
-			for(int dir : moveDirs) {
-				int ny = y + dy[dir];
-				int nx = x + dx[dir];
-				if(OOB(ny,nx)) {
-					if(ny<=0)
-						continue;
-					else
-						return NO_POS;
-				}
-				
-				if(board[ny][nx]!=0)
-					return NO_POS;
-			}
-			
-			if(tempDir == 2) {
-				return new Tuple(y,x,exitDir);
-			}
-			
-			int moveDir = (exitDir + tempDir) % 4;
-			return getNxtPos(y,x,moveDir,new int[]{1,2,3}, 2);
-			
-		}
-		
-		
-		private void update(Tuple nxt) {
-			cy = nxt.first;
-			cx = nxt.second;
-			exitDir = nxt.third;
-			exit.y = cy + dy[exitDir];
-			exit.x = cx + dx[exitDir];
-			fairy.first = cy;
-			fairy.second = cx;
-		}
-		
-		private boolean OOR() {
-			if(OOB(cy,cx))
-				return true;
-			
-			for(int dir = 0; dir < 4;dir++) {
-				int ny = cy + dy[dir];
-				int nx = cx + dx[dir];
-				if(OOB(ny,nx))
-					return true;
-			}
-			return false;
-		}
-		
-		public boolean move() {
-			Tuple nxt = null;
-			while(true) {
-				nxt = getNxtPos(cy,cx,exitDir, new int[] {1,2,3}, 2);
-				if(nxt != NO_POS) {
-					update(nxt);
-				}else if((nxt = getNxtPos(cy,cx,exitDir, new int[]{0,2,3},3)) != NO_POS) {
-					update(nxt);
-				}else if((nxt = getNxtPos(cy,cx,exitDir, new int[]{0,1,2},1)) != NO_POS) {
-					update(nxt);
-				}
-				else
-					break;
-			}
-			if(OOR()) {
-				return true;
-			}
+    private static final int MAX_L = 70;
 
-			board[cy][cx] = id;
-			for(int dir = 0; dir<4;dir++) {
-				int y = cy + dy[dir];
-				int x = cx + dx[dir];
-				board[y][x] = id;
-			}
-			return false;
-		}
-		
-		public int fairyMove() {
-			Queue<Tuple> q = new LinkedList<>();
-			boolean visited[][]= new boolean[r+1][c+1];
-			q.add(fairy);
-			visited[fairy.first][fairy.second] = true;
-			int ret = fairy.first;
-			Pair useExit = exit;
-			
-			while(!q.isEmpty()) {
-				Tuple cur = q.poll();
-				ret = Math.max(ret,  cur.first);
-				useExit = golems.get(cur.third-1).exit;
-				
-				for(int dir = 0; dir<4;dir++) {
-					int ny = cur.first + dy[dir];
-					int nx = cur.second + dx[dir];
-					if(OOB(ny,nx) || board[ny][nx] == 0 || visited[ny][nx])
-						continue;
-					
-					if(board[ny][nx] == cur.third || cur.isSame(useExit.y, useExit.x)) {
-						visited[ny][nx] = true;
-						q.add(new Tuple(ny,nx, board[ny][nx]));
-					}
-				}
-			}
-			
-			return ret;
-		}
-		
-		public void clear() {
-			if(OOB(cy,cx))
-				return;
-			board[cy][cx] = 0;
-			for(int dir = 0; dir < 4; dir++) {
-				int ny = cy + dy[dir];
-				int nx = cx + dx[dir];
-				if(OOB(ny,nx))
-					continue;
-				board[ny][nx] = 0;
-			}
-		}
-		
-		public String toString() {
-			return cy+" "+cx+" "+exitDir;
-		}
-	}
-	
-	static List<Golem> golems = new ArrayList<>();
-	static int ans;
-	static void clear() {
-		for(int i = 0; i < golems.size()-1;i++) {
-			golems.get(i).clear();
-		}
-	}
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		st = new StringTokenizer(br.readLine());
-		r = Integer.parseInt(st.nextToken());
-		c = Integer.parseInt(st.nextToken());
-		k = Integer.parseInt(st.nextToken());
-		board = new int[r+1][c+1];
-		for(int i = 1; i <= k ;i++) {
-			st = new StringTokenizer(br.readLine());
-			int x = Integer.parseInt(st.nextToken());
-			int dir = Integer.parseInt(st.nextToken());
-			
-			Golem golem = new Golem(i, -1,x, dir);
-			boolean oor = golem.move();
-			golems.add(golem);
-			if(oor) {
-				clear();
-				continue;
-			}
-			
-			
-			int score = golem.fairyMove();
-			ans += score;
-		}
-		System.out.println(ans);
-	}
+    private static int R, C, K; // 행, 열, 골렘의 개수를 의미합니다
+//    private static int[][] board = new int[MAX_L + 3][MAX_L]; // 실제 숲을 [3~R+2][0~C-1]로 사용하기위해 행은 3만큼의 크기를 더 갖습니다
+    private static int[] dy = {-1, 0, 1, 0}, dx = {0, 1, 0, -1};
+//    private static boolean[][] isExit = new boolean[MAX_L + 3][MAX_L]; // 해당 칸이 골렘의 출구인지 저장합니다
+    private static int answer = 0; // 각 정령들이 도달할 수 있는 최하단 행의 총합을 저장합니다
+    static boolean isExit[][];
+    // (y, x)가 숲의 범위 안에 있는지 확인하는 함수입니다
+    private static boolean inRange(int y, int x) {
+        return 3 < y && y <= R + 3 && 0 < x && x <= C;
+    }
+
+    // 숲에 있는 골렘들이 모두 빠져나갑니다
+    private static void resetMap() {
+        for (int i = 1; i <= R + 3; i++) {
+            for (int j = 1; j <= C; j++) {
+                board[i][j] = 0;
+                isExit[i][j] = false;
+            }
+        }
+    }
+
+    // 골렘의 중심이 y, x에 위치할 수 있는지 확인합니다.
+    // 북쪽에서 남쪽으로 내려와야하므로 중심이 (y, x)에 위치할때의 범위와 (y-1, x)에 위치할떄의 범위 모두 확인합니다
+    private static boolean canGo(int y, int x) {
+//	    boolean flag = 0 <= x - 1 && x + 1 < C && y + 1 < R + 3;
+    	boolean flag = 0 < x-1 && x+1<=C && y+1 <= R+3;
+        flag = flag && (board[y - 1][x - 1] == 0);
+        flag = flag && (board[y - 1][x] == 0);
+        flag = flag && (board[y - 1][x + 1] == 0);
+        flag = flag && (board[y][x - 1] == 0);
+        flag = flag && (board[y][x] == 0);
+        flag = flag && (board[y][x + 1] == 0);
+        flag = flag && (board[y + 1][x] == 0);
+        return flag;
+    }
+
+    // 정령이 움직일 수 있는 모든 범위를 확인하고 도달할 수 있는 최하단 행을 반환합니다
+    private static int bfs(int y, int x) {
+        int result = y;
+        Queue<int[]> q = new LinkedList<>();
+//        boolean[][] visit = new boolean[MAX_L + 3][MAX_L];
+        boolean visit[][] = new boolean[R+3+1][C+1];
+        q.offer(new int[]{y, x});
+        visit[y][x] = true;
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            for (int k = 0; k < 4; k++) {
+                int ny = cur[0] + dy[k], nx = cur[1] + dx[k];
+                // 정령의 움직임은 골렘 내부이거나
+                // 골렘의 탈출구에 위치하고 있다면 다른 골렘으로 옮겨 갈 수 있습니다
+                if (inRange(ny, nx) && !visit[ny][nx] && (board[ny][nx] == board[cur[0]][cur[1]] || (board[ny][nx] != 0 && isExit[cur[0]][cur[1]]))) {
+                    q.offer(new int[]{ny, nx});
+                    visit[ny][nx] = true;
+                    result = Math.max(result, ny);
+                }
+            }
+        }
+        return result;
+    }
+
+    // 골렘id가 중심 (y, x), 출구의 방향이 d일때 규칙에 따라 움직임을 취하는 함수입니다
+    // 1. 남쪽으로 한 칸 내려갑니다.
+    // 2. (1)의 방법으로 이동할 수 없으면 서쪽 방향으로 회전하면서 내려갑니다.
+    // 3. (1)과 (2)의 방법으로 이동할 수 없으면 동쪽 방향으로 회전하면서 내려갑니다.
+    private static void down(int y, int x, int d, int id) {
+        if (canGo(y + 1, x)) {
+            // 아래로 내려갈 수 있는 경우입니다
+            down(y + 1, x, d, id);
+        } else if (canGo(y + 1, x - 1)) {
+            // 왼쪽 아래로 내려갈 수 있는 경우입니다
+            down(y + 1, x - 1, (d + 3) % 4, id);
+        } else if (canGo(y + 1, x + 1)) {
+            // 오른쪽 아래로 내려갈 수 있는 경우입니다
+            down(y + 1, x + 1, (d + 1) % 4, id);
+        } else {
+            // 1, 2, 3의 움직임을 모두 취할 수 없을떄 입니다.
+            if (!inRange(y-1, x-1) || !inRange(y+1, x+1)) {
+                // 숲을 벗어나는 경우 모든 골렘이 숲을 빠져나갑니다
+                resetMap();
+            } else {
+                // 골렘이 숲 안에 정착합니다
+                board[y][x] = id;
+                for (int k = 0; k < 4; k++)
+                    board[y + dy[k]][x + dx[k]] = id;
+                // 골렘의 출구를 기록하고
+                isExit[y + dy[d]][x + dx[d]] = true;
+                // bfs를 통해 정령이 최대로 내려갈 수 있는 행를 계산하여 누적합합니다
+//                answer += bfs(y, x) - 3 + 1;
+                int ret = bfs(y,x) - 3 ;
+//                System.out.println(ret);
+                answer += ret;
+            }
+        }
+    }
+
+    static StringTokenizer st;
+    static int board[][];
+    public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	st = new StringTokenizer(br.readLine());
+    	R = Integer.parseInt(st.nextToken());
+    	C = Integer.parseInt(st.nextToken());
+    	K = Integer.parseInt(st.nextToken());
+    	board = new int[R+3+1][C+1];
+    	isExit = new boolean[R+3+1][C+1];
+        for (int id = 1; id <= K; id++) { // 골렘 번호 id
+        	st = new StringTokenizer(br.readLine());
+        	int x = Integer.parseInt(st.nextToken());
+        	int d = Integer.parseInt(st.nextToken());
+            down(0, x, d, id);
+        }
+        System.out.println(answer);
+    }
 }
