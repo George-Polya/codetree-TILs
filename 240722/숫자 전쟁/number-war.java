79dd9cc6c7a162a11a01717cbbce702c92c47725
Kim Hee Su <wlskaka4@gmail.com>
2024-07-22 22:21:26 +0900

    Title: 숫자 전쟁, Time: 172ms, Memory: 13MB, Status: Passed - CodeTree

--- 240722/숫자 전쟁/number-war.java
+++ 240722/숫자 전쟁/number-war.java
@@ -1,68 +1,51 @@
 import java.io.*;
-import java.time.*;
-import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class Main {
 	static StringTokenizer st;
-	static int n,A[],B[],dp[][];
+	static int n, A[], B[], dp[][];
 	
-	static void printBoard(int board[][]) {
-		for(int y= 1;y<=n; y++) {
-			for(int x=1; x<=n; x++) {
-				System.out.printf("%3d", board[y][x]);
-			}
-			System.out.println();
-		}
+	static int solve(int a, int b) {
+		if(a == n || b == n) // 끝까지 도달하면 더 이상 점수를 얻을 수 없음
+			return 0;
+		
+		if(dp[a][b] != -1) // 이미 계산한 적이 있으면 저장된 값 반환
+			return dp[a][b];
+		
+		dp[a][b] = 0; // 초기화
+		
+		// p1의 카드가 더 작은 경우, p1 카드만 버림
+		if(A[a] < B[b]) 
+			dp[a][b] = Math.max(dp[a][b], solve(a+1, b));
+		
+		// p1의 카드가 더 큰 경우, p2의 카드 버리고 p1 점수 얻음
+		if(A[a] > B[b])
+			dp[a][b] = Math.max(dp[a][b], solve(a, b+1) + B[b]);
+		
+		// 둘 다 카드를 버림
+		dp[a][b] = Math.max(dp[a][b], solve(a+1, b+1));
+		
+		return dp[a][b];
 	}
 	
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
-		A = new int[n+1];
-		B = new int[n+1];
+		A = new int[n];
+		B = new int[n];
 		
 		st = new StringTokenizer(br.readLine());
-		for(int i=1;i<=n;i++)
+		for(int i = 0; i < n; i++)
 			A[i] = Integer.parseInt(st.nextToken());
 		st = new StringTokenizer(br.readLine());
-		for(int i=1;i<=n;i++)
+		for(int i = 0; i < n; i++)
 			B[i] = Integer.parseInt(st.nextToken());
 		
-		
-		/*
-		 * dp[a][b] : p1이 a번째카드까지 버리고, p2가 b번째카드까지 버렸을때 얻을 수 있는 최대 점수 
-		 */
-		dp = new int[n+1][n+1]; 
-		for(int y=0; y<=n; y++)
+		dp = new int[n][n]; 
+		for(int y = 0; y < n; y++)
 			Arrays.fill(dp[y], -1);
 		
-		dp[0][0] = 0;
-		
-		for(int a = 0; a<n; a++) {
-			for(int b = 0; b<n; b++) {
-				if(dp[a][b] == -1) 
-					continue;
-				
-				if(A[a+1] < B[b+1]) // p1의 카드가 더 작은 경우
-					dp[a+1][b] = Math.max(dp[a+1][b], dp[a][b]);
-				if(A[a+1] > B[b+1])
-					dp[a][b+1] = Math.max(dp[a][b+1], dp[a][b] + B[b+1]);
-				
-				// 카드 버리기
-				dp[a+1][b+1] = Math.max(dp[a+1][b+1], dp[a][b]);
-				
-			}
-		}
-		
-//		printBoard(dp);
-		
-		int ans = 0;
-		for(int i = 0; i<=n;i++) {
-			ans = Math.max(ans, dp[i][n]);
-			ans = Math.max(ans, dp[n][i]);
-		}
+		int ans = solve(0, 0);
 		System.out.println(ans);
-		
 	}
 }
