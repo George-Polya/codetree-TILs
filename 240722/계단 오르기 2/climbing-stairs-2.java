fa6a8e15b19aa39709d7ffcef8c76d430bf9ea0f
Kim Hee Su <wlskaka4@gmail.com>
2024-07-22 19:46:32 +0900

    Title: 계단 오르기 2, Time: 181ms, Memory: 9MB, Status: Passed - CodeTree

--- 240722/계단 오르기 2/climbing-stairs-2.java
+++ 240722/계단 오르기 2/climbing-stairs-2.java
@@ -28,28 +28,36 @@ public class Main {
 		}
 		
 		dp = new int[3+1][n+1];
-		for(int i=0;i<=3;i++) {
-			Arrays.fill(dp[i], INT_MIN);
-		}
+//		for(int i=0;i<=3;i++) {
+//			Arrays.fill(dp[i], INT_MIN);
+//		}
 		
+		dp[1][1] = arr[1];
 		dp[0][2] = arr[2];
 		dp[2][2] = arr[1] + arr[2];
-		dp[1][1] = arr[1];
 		
-		for(int cnt=1; cnt<=3;cnt++) {
-			for(int i = 3; i<=n; i++) {
-//				System.out.printf("%d %d\n", dp[cnt-1][i-1], dp[cnt][i-2]);
-				if(cnt <= i)
-					dp[cnt][i] = Math.max(dp[cnt-1][i-1], dp[cnt][i-2]) + arr[i];
+//		for(int cnt=1; cnt<=3;cnt++) {
+//			for(int i = 2; i<=n; i++) {
+////				System.out.printf("%d %d\n", dp[cnt-1][i-1], dp[cnt][i-2]);
+//				dp[cnt][i] = Math.max(dp[cnt-1][i-1], dp[cnt][i-2]) + arr[i];
+//			}
+//		}
+		
+		for(int i = 3; i<=n;i++) {
+			for(int cnt = 0; cnt<=3; cnt++) {
+				if(dp[cnt][i-2] != 0)
+					dp[cnt][i] = Math.max(dp[cnt][i], dp[cnt][i-2] + arr[i]);
+				if(cnt >0 && dp[cnt - 1][i-1] != 0)
+					dp[cnt][i] = Math.max(dp[cnt][i], dp[cnt-1][i-1]+arr[i]);
 			}
 		}
+		
 		
 //		printBoard(dp);
 		
 		int ans = INT_MIN;
 		for(int cnt = 0; cnt<=3;cnt++) {
-			for(int i = 0; i<=n; i++)
-				ans = Math.max(ans, dp[cnt][i]);
+			ans = Math.max(ans, dp[cnt][n]);
 		}
 		System.out.println(ans);
 	}
