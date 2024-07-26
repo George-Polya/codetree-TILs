a090996f07d592a10c11243ad075f6dd20878cba
Kim Hee Su <wlskaka4@gmail.com>
2024-07-26 14:26:46 +0900

    Title: 계단 수, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240726/계단 수/stair-number.java
+++ 240726/계단 수/stair-number.java
@@ -18,12 +18,14 @@ public class Main {
 		for(int i = 2; i<=n; i++) {
 			for(int k = 0; k <=9;k++) {
 				if(k == 0) {
-					dp[i][k] = (dp[i][k] + dp[i-1][k+1]) % MOD;
+					dp[i][k] = (dp[i][k] % MOD + dp[i-1][k+1] % MOD);
 				}else if(k == 9) {
-					dp[i][k] = (dp[i][k] + dp[i-1][k-1]) % MOD;
+					dp[i][k] = (dp[i][k] % MOD + dp[i-1][k-1] % MOD); 
 				}else {
-					dp[i][k] = (dp[i][k] + dp[i-1][k+1] + dp[i-1][k-1]) % MOD;
+					dp[i][k] = (dp[i][k] % MOD + dp[i-1][k+1] % MOD+ dp[i-1][k-1] % MOD); 
 				}
+				
+				dp[i][k] %= MOD;
 			}
 		}
 		
