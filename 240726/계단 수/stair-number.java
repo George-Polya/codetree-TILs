13e545dac3768e351d29ffe29b5e43e42055006b
Kim Hee Su <wlskaka4@gmail.com>
2024-07-26 14:28:26 +0900

    Title: 계단 수, Time: 101ms, Memory: 8MB, Status: Passed - CodeTree

--- 240726/계단 수/stair-number.java
+++ 240726/계단 수/stair-number.java
@@ -4,13 +4,13 @@ import java.util.*;
 
 public class Main {
 	static int n;
-	static long dp[][];
+	static int dp[][];
 	static int MOD = (int)1e9+7;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
 		
-		dp = new long[n+1][10];
+		dp = new int[n+1][10];
 		
 		for(int i=1;i<=9;i++)
 			dp[1][i] = 1;
@@ -18,20 +18,18 @@ public class Main {
 		for(int i = 2; i<=n; i++) {
 			for(int k = 0; k <=9;k++) {
 				if(k == 0) {
-					dp[i][k] = (dp[i][k] % MOD + dp[i-1][k+1] % MOD);
+					dp[i][k] = (dp[i][k] + dp[i-1][k+1]) % MOD;
 				}else if(k == 9) {
-					dp[i][k] = (dp[i][k] % MOD + dp[i-1][k-1] % MOD); 
+					dp[i][k] = (dp[i][k] + dp[i-1][k-1]) % MOD;
 				}else {
-					dp[i][k] = (dp[i][k] % MOD + dp[i-1][k+1] % MOD+ dp[i-1][k-1] % MOD); 
+					dp[i][k] = (dp[i][k] + dp[i-1][k+1] + dp[i-1][k-1]) % MOD;
 				}
-				
-				dp[i][k] %= MOD;
 			}
 		}
 		
 		long ans = 0;
 		for(int i = 0; i<=9;i++)
-			ans = (ans + dp[n][i]) % MOD;
+			ans =  (ans + dp[n][i]) % MOD;
 		System.out.println(ans);
 		
 	}
