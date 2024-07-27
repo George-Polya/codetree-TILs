16a2a10e07c506de652d2dcd5b2cf23d2c7a61e4
Kim Hee Su <wlskaka4@gmail.com>
2024-07-27 19:30:38 +0900

    Title: 신전 탐험하기 2, Time: 96ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240727/신전 탐험하기 2/explore-temple-2.java
+++ 240727/신전 탐험하기 2/explore-temple-2.java
@@ -23,17 +23,29 @@ public class Main {
 		dp[1][MID][MID] = board[1][MID];
 		dp[1][RIGHT][RIGHT] = board[1][RIGHT];
 		
-		for(int i = 2; i<=n; i++) {
-			for(int a = 0; a<3;a++) {
-				for(int b = 0; b<3; b++) {
-					// if(a == b)
-					// 	continue;
-					
-					for(int c= 0; c<3; c++) {
+//		for(int i = 2; i<=n; i++) {
+//			for(int a = 0; a<3;a++) {
+//				for(int b = 0; b<3; b++) {
+//					if(a == b)
+//						continue;
+//					
+//					for(int c= 0; c<3; c++) {
+//						if(a == c)
+//							continue;
+//						
+//						dp[i][a][c] = Math.max(dp[i][a][c], dp[i-1][b][c] + board[i][a]);
+//					}
+//				}
+//			}
+//		}
+		
+		for(int i = 1; i < n; i++) {
+			for(int a= 0; a < 3; a++) {
+				for(int b= 0; b<3; b++) {
+					for(int c= 0; c<3;c++) {
 						if(b == c)
 							continue;
-						
-						dp[i][a][c] = Math.max(dp[i][a][c], dp[i-1][b][c] + board[i][a]);
+						dp[i+1][a][c] = Math.max(dp[i+1][a][c], dp[i][a][b] + board[i][c]);
 					}
 				}
 			}
@@ -41,8 +53,11 @@ public class Main {
 		
 		int ans = 0;
 		for(int y=0; y<3; y++) {
-			for(int x= 0; x<3; x++)
+			for(int x= 0; x<3; x++) {
+				if(y == x)
+					continue;
 				ans = Math.max(ans, dp[n][y][x]);
+			}
 		}
 		System.out.println(ans);
 	}
