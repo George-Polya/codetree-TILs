8b9f5ac31c9720636351750800be128d99567375
Kim Hee Su <wlskaka4@gmail.com>
2024-07-29 12:01:41 +0900

    Title: 숫자 전쟁, Time: 90ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240729/숫자 전쟁/number-war.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+	static StringTokenizer st;
+	static int n, A[],B[], dp[][];
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		A = new int[n+1];
+		B = new int[n+1];
+		dp = new int[n+1][n+1];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 1; i<=n; i++)
+			A[i] = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i = 1; i<=n; i++)
+			B[i] = Integer.parseInt(st.nextToken());
+		
+		for(int a = 0; a< n; a++) {
+			for(int b=0; b<n; b++) {
+				if(A[a+1] < B[b+1])
+					dp[a+1][b] = Math.max(dp[a+1][b], dp[a][b]);
+				else if(A[a+1]>B[b+1])
+					dp[a][b+1] = Math.max(dp[a][b+1], dp[a][b] + B[b+1]);
+				else
+					dp[a+1][b+1] = Math.max(dp[a+1][b+1], dp[a][b]);
+				
+				dp[a+1][b+1] = Math.max(dp[a+1][b+1], dp[a][b]);
+			}
+		}
+		
+		int ans = 0;
+//		for(int y=0;y<=n; y++) {
+//			for(int x=0; x<=n; x++) {
+//				System.out.print(dp[y][x] +" ");
+//				ans = Math.max(ans, dp[y][x]);
+//			}
+//			System.out.println();
+//		}
+		
+		for(int y = 1; y<=n; y++)
+			ans = Math.max(ans, dp[y][n]);
+		System.out.println(ans);
+	}
+}
