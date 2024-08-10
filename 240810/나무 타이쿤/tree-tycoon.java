82987a1ec9d6d28b7795ab59ecd1f9eb97afad34
Kim Hee Su <wlskaka4@gmail.com>
2024-08-10 22:04:40 +0900

    Title: 나무 타이쿤, Time: 120ms, Memory: 9MB, Status: Passed - CodeTree

--- 240810/나무 타이쿤/tree-tycoon.java
+++ 240810/나무 타이쿤/tree-tycoon.java
@@ -1,165 +1,163 @@
-import java.io.*;
 import java.util.*;
-
+import java.io.*;
 
 public class Main {
-	static int n,m;
-	static int board[][];
-	static StringTokenizer st;
-	static int dy[] = {0,-1,-1,-1,0,1,1,1};
-	static int dx[] = {1,1,0,-1,-1,-1,0,1};
-	static boolean OOB(int y, int x) {
-		return y<=0 || y>n || x<=0 || x>n;
-	}
-	static class Pair{
-		int y,x;
-		public Pair(int y,int x) {
-			this.y = y;
-			this.x = x;
-		}
-		
-		public String toString() {
-			return y+" "+x;
-		}
-	}
-	static int total;
-	static Deque<Pair> fertils = new ArrayDeque<>();
-	static void printBoard(int board[][]) {
-		for(int y=1; y<=n; y++) {
-			for(int x=1; x<=n; x++) {
-				System.out.printf("%3d", board[y][x]);
-			}
-			System.out.println();
-		}
-	}
-	
-	static void move(Pair pair, int dir, int dist) {
-		int y = pair.y;
-		int x = pair.x;
+    static class moveRule{
+        int dir;
+        int len;
+        public moveRule(int dir, int len){
+            this.dir = dir;
+            this.len = len;
+        }
+    }
+    static class Point{
+        int r;
+        int c;
+        public Point(int r, int c){
+            this.r = r;
+            this.c = c;
+        }
 
-		int ny = y + dy[dir] * (dist % n);
-		int nx = x + dx[dir] * (dist % n);
-		
-//		while(OOB(ny,nx)) {
-//			if(ny <= 0)
-//				ny += n;
-//			else if(ny > n)
-//				ny -= n;
-//			
-//			if(nx <= 0)
-//				nx += n;
-//			else if(nx > n)
-//				nx -= n;
-//				
-//		}
-		
-		
-		ny = ((ny - 1 + n) % n) + 1;
-		nx = ((nx - 1 + n) % n) + 1;
-		
-//		pair.y = ny;
-//		pair.x = nx;
-		
-		fertils.addLast(new Pair(ny,nx));
-		
-	}
-	
-	static void grow(int y, int x) {
-		total++;
-		board[y][x] += 1;
-	}
-	static void extraGrow(int y, int x) {
-		int cnt = 0;
-		for(int dir = 1; dir <8; dir+=2) {
-			int ny = y + dy[dir];
-			int nx = x + dx[dir];
-			if(OOB(ny,nx))
-				continue;
-			if(board[ny][nx] >= 1)
-				cnt++;
-		}
-		total+=cnt;
-		board[y][x] += cnt;
-	}
-	
-	static boolean fertilized[][];
-	
-	static void simulate(int dir, int dist) {
-		
-		// 영양제의 이동과 나무의 성장 
-//		for(int i = 0; i < fertils.size(); i++) {
-//			
-//			// 이동 
-//			Pair pair = fertils.get(i);
-//			move(pair, dir, dist);
-//			fertilized[pair.y][pair.x] = true;
-//			// 투입해서 성장 
-//			grow(pair.y, pair.x);
-//			
-//		}
-		
-		int size = fertils.size();
-		for(int i = 0; i< size;i++) {
-			Pair pair = fertils.pollFirst();
-			move(pair, dir, dist);
-			grow(pair.y, pair.x);
-			fertils.addFirst(pair);
-		}
-		
-		
-		// 추가 성장
-		for(int i = 0 ; i < size;i++) {
-			Pair pair = fertils.pollFirst();
-			extraGrow(pair.y,pair.x);
-		}
-		
-//		fertils.clear(); // 영양제 소멸 
-		
-		// 영양제를 투입한 위치를 제외하고 높이 2이상인 리브로수 제거 후 특수영양제 추가 
-		for(int y=1; y<=n; y++) {
-			for(int x=1; x<=n; x++) {
-				if(fertilized[y][x]) {
-					fertilized[y][x] = false;
-					continue;
-				}
-				if(board[y][x] >= 2) {
-					board[y][x] -= 2;
-					fertils.add(new Pair(y,x));
-					total -= 2;
-				}
-			}
-		}
-	
-		
-	}
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		st = new StringTokenizer(br.readLine());
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		board = new int[n+1][n+1];
-		fertilized = new boolean[n+1][n+1];
-		for(int y=1; y<=n; y++) {
-			st = new StringTokenizer(br.readLine());
-			for(int x=1; x<=n; x++) {
-				board[y][x] = Integer.parseInt(st.nextToken());
-				total += board[y][x];
-			}
-		}
-		
-		
-		for(int y = n-1; y<=n; y++) {
-			for(int x=1; x<=2;x++)
-				fertils.add(new Pair(y,x));
-		}
-		
-		for(int turn = 1; turn<=m; turn++) {
-			st = new StringTokenizer(br.readLine());
-			int d = Integer.parseInt(st.nextToken()) - 1;
-			int p = Integer.parseInt(st.nextToken());
-			simulate(d,p);
-		}
-		
-		System.out.println(total);
-	}
+        @Override
+        public boolean equals(Object o) {
+            if(this == o) return true;
+            if(o == null || getClass() != o.getClass()) return false;
+            Point point = (Point) o;
+            return this.r == point.r && this.c == point.c;
+        }
+    }
+    static int N, M;
+    static int[][] map;
+    static Queue<moveRule> moveQueue = new LinkedList<>();
+    static Queue<Point> nutritionQueue = new LinkedList<>();
+    static int answer;
+//    static int[] dx = {-1, -1, 1, 1};
+//    static int[] dy = {-1, 1, 1, -1};
+    static int dx[] = {0,-1,-1,-1,0,1,1,1};
+    static int dy[] = {1,1,0,-1,-1,-1,0,1};
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][N];
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < N ;j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for(int i = 0; i < M; i++){
+            st = new StringTokenizer(br.readLine());
+            int d = Integer.parseInt(st.nextToken()) - 1;
+            int l = Integer.parseInt(st.nextToken());
+            moveQueue.offer(new moveRule(d,l));
+        }
+
+        answer = 0;
+
+        simulate();
+
+        System.out.println(answer);
+    }
+
+    private static void simulate(){
+        initialNutrition();
+
+        for(int i = 0; i < M; i++){
+            moveRule thisRound = moveQueue.poll();
+            moveNutrition(thisRound);
+            plusLibro();
+            growLibro();
+            prepareNextYear();
+        }
+
+        getAnswer();
+    }
+
+    private static void initialNutrition(){
+        for(int i = N-2; i<= N-1; i++){
+            for(int j = 0; j <= 1; j++){
+                nutritionQueue.offer(new Point(i,j));
+            }
+        }
+    }
+
+    private static void moveNutrition(moveRule thisRound){
+        // 영쟝제가 뿌려져있는 땅의 개수만큼 돌면서
+        for(int i = 0; i < nutritionQueue.size(); i++){
+            Point now = nutritionQueue.poll();
+            int nr = now.r + dx[thisRound.dir] * thisRound.len % N;
+            int nc = now.c + dy[thisRound.dir] * thisRound.len % N;
+            
+            nr = (nr + N) % N;
+            nc = (nc + N) % N;
+            
+            
+            nutritionQueue.offer(new Point(nr,nc));
+            
+            
+        }
+    }
+
+    private static void growLibro(){
+        for(Point now: nutritionQueue){
+            for(int d = 1; d < 8; d+=2){
+                int nr = now.r + dx[d];
+                int nc = now.c + dy[d];
+
+                if(isInRange(nr, nc) && map[nr][nc] != 0){
+                    map[now.r][now.c] += 1;
+                }
+            }
+        }
+    }
+
+    private static void plusLibro(){
+        for(Point now: nutritionQueue){
+            map[now.r][now.c] += 1;
+        }
+    }
+
+    private static void prepareNextYear(){
+        int qSize = nutritionQueue.size();
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                if(nutritionQueue.contains(new Point(i,j))) continue;
+                if(map[i][j] >= 2){
+                    map[i][j] -= 2;
+                    nutritionQueue.offer(new Point(i,j));
+                }
+            }
+        }
+        for(int i = 0; i < qSize; i++){
+            nutritionQueue.poll();
+        }
+    }
+
+    private static void getAnswer(){
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                answer += map[i][j];
+            }
+        }
+    }
+
+    // 범위 밖으로 벗어나는지 확인하는 함수
+    private static boolean isInRange(int r, int c){
+        return r >= 0 && c >= 0 && r < N && c < N;
+    }
+
+    private static void printMap(){
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                System.out.print(map[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
