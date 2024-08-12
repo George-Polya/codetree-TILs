8d687d709acb6a4c8b85226d5312f524b9feec88
Kim Hee Su <wlskaka4@gmail.com>
2024-08-12 17:25:58 +0900

    Title: 포탑 부수기, Time: 162ms, Memory: 14MB, Status: Passed - CodeTree

--- 240812/포탑 부수기/destroy-the-turret.java
+++ 240812/포탑 부수기/destroy-the-turret.java
@@ -1,243 +1,286 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.*;
-
-/**
- * [문제 해결 프로세스]
- * 1. 가장 약한 포탑 찾기 -> 공격자 선정
- * 2. 가장 강한 포탑 찾기 -> 공격
- * 3. 레이저 공격 -> 우하좌상, 가장 강한 포탑 외 해당 경로의 포탑 역시 공격 대상이 됨
- * 4. 포탄 공격 -> 레이저 공격 불가 시.
- * -> 가장 강한 포탑의 공격력을 출력
- */
+import java.io.*;
 
 public class Main {
-    static int N, M, K;
-    static int[][] arr, cntArr;
-    static boolean[][] relative;
-    static int idx = 0;
-    static int turn;
-    public static void main(String[] args) throws IOException {
+	static StringTokenizer st;
+	static Turret board[][];
+	static int n,m,k;
+	
+	
+	static class Turret implements Comparable<Turret>{
+		int power, time, y,x;
+		
+		public Turret(int power, int time, int y, int x) {
+			this.time = time;
+			this.power = power;
+			this.y = y;
+			this.x = x;
+		}
+		
+		public int compareTo(Turret t) {
+			if(power != t.power)
+				return power - t.power;
+			if(time != t.time)
+				return t.time - time;
+			if( (y+x) != (t.y + t.x))
+				return (t.y + t.x) - (y + x);
+			return t.x - x;
+		}
+		
+		public boolean isWeaker(Turret t) {
+			if(power != t.power)
+				return power < t.power;
+			if(time != t.time)
+				return t.time < time;
+			if( (y+x) != (t.y + t.x))
+				return (t.y + t.x) < (y + x);
+			return t.x < x;
+		}
+		
+		public boolean isStronger(Turret t) {
+			if(power != t.power)
+				return power > t.power;
+			if(time != t.time)
+				return t.time > time;
+			if( (y+x) != (t.y + t.x))
+				return (t.y + t.x) > (y + x);
+			return t.x > x;
+		}
+		
+		public String toString() {
+			return String.format("(%d, %d), power: %d, time: %d", y,x,power,time);
+		}
+	}
+	
+	static Turret STORNGEST = new Turret(5001,-1,-1,-1);
+	static Turret WEAKEST = new Turret(-1,1001,12,12);
+	
+	static class Pair{
+		int y,x;
+		public Pair(int y,int x) {
+			this.y = y;
+			this.x = x;
+		}
+		public String toString() {
+			return y+" "+x;
+		}
+		
+		public boolean isSame(int y,int x) {
+			return this.y == y && this.x == x;
+		}
+	}
+	static Pair NO_POS = new Pair(-1,-1);
+	
+	static void printBoard(Turret board[][]) {
+		for(int y=1; y<=n; y++) {
+			for(int x=1; x<=m; x++) {
+				System.out.printf("%3d", board[y][x].power);
+			}
+			System.out.println();
+		}
+	}
+	
+	static Turret BROKEN = new Turret(0,-1,-1,-1);
+//	static ArrayList<Turret> turrets;
+	static Turret attacker, target;
+	static void init() {
+//		turrets = new ArrayList<>();
+//
+//		for(int y=1; y<=n; y++) {
+//			for(int x=1; x<=m; x++) {
+//				involved[y][x] = false;
+//				if(board[y][x] == BROKEN)
+//					continue;
+//				turrets.add(board[y][x]);
+//			}
+//		}
+//		
+//		Collections.sort(turrets);
+//		attacker = turrets.get(0);
+//		target = turrets.get(turrets.size() - 1);
+		attacker = STORNGEST;
+		target = WEAKEST;
+		for(int y=1; y<=n; y++) {
+			for(int x=1; x<=m; x++) {
+				involved[y][x] = false;
+				if(board[y][x] == BROKEN)
+					continue;
+				if(board[y][x].isWeaker(attacker)) {
+					attacker = board[y][x];
+				}
+				
+				if(board[y][x].isStronger(target)) {
+					target = board[y][x];
+				}
+			}
+		}
+		
+	}
+	
+	static boolean involved[][];
+	static int dy[] = {0,1,0,-1,-1,1,1,-1};
+	static int dx[] = {1,0,-1,0,1,1,-1,-1};
+	static boolean OOB(int y,int x) {
+		return y<= 0 || y>n || x<=0 || x>m;
+	}
+	
+	static void attack() {
+		
+		// 최단 경로 존재 판단  
+		boolean canLaser = false;
+		Queue<Pair> q = new LinkedList<>();
+		q.add(new Pair(attacker.y, attacker.x));
+		Pair prev[][] = new Pair[n+1][m+1];
+		prev[attacker.y][attacker.x] = NO_POS;
+		while(!q.isEmpty()) {
+			Pair cur = q.poll();
+			
+			if(cur.isSame(target.y, target.x)) {
+				canLaser = true;
+				break;
+			}
+			
+			for(int dir = 0; dir< 4; dir++) {
+				int ny = cur.y + dy[dir];
+				int nx = cur.x + dx[dir];
+				ny = ((ny - 1 + n) % n) + 1;
+				nx = ((nx - 1 + m) % m) + 1;
+				
+				if(prev[ny][nx] != null || board[ny][nx] == BROKEN)
+					continue;
+				prev[ny][nx] = cur;
+//				visited[ny][nx] = true;
+				q.add(new Pair(ny,nx));
+			}
+		}
+		
+		// 최단경로로 이동가능하다면 레이저 공격 
+		int power = attacker.power + (n + m);
+		if(canLaser) {
+			Pair pair = prev[target.y][target.x];
+			
+			// 레이저 경로에 있는 포탑들은 증가된 공격력의 절반만큼의 공격을 받음 
+			while(pair != NO_POS) {
+//				System.out.println(pair);
+				involved[pair.y][pair.x] = true;
+				board[pair.y][pair.x].power -= power / 2;
+				pair = prev[pair.y][pair.x];
+			}
+			
+			// 공격대상은 증가된 공격력만큼의 공격을 받음 
+			target.power -= power;
+			involved[target.y][target.x] = true;
+			
+			attacker.power = power;
+			involved[attacker.y][attacker.x] = true;
+			attacker.time = turn;
+			return;
+		}
+		
+		
+		/*
+		 * 공격대상 주위 8개의 방향에 있는 포탑도 피해를 받는다 
+		 * 공격자는 공격에 영향을 받지 않는다
+		 * 격자 밖을 벗어나면  
+		 */
+		int y = target.y;
+		int x = target.x;
+		
+		involved[y][x] = true;
+		target.power -= power;
+		
+		for(int dir = 0; dir < 8; dir++) {
+			int ny = y + dy[dir];
+			int nx = x + dx[dir];
+			ny = ((ny - 1 + n) % n ) + 1;
+			nx = ((nx - 1 + m) % m ) + 1;
+			
+			// 공격자는 공격에 영향받지 않는다 
+			if(board[ny][nx] == BROKEN || (ny == attacker.y && nx == attacker.x))
+				continue;
+			involved[ny][nx] = true;
+			board[ny][nx].power -= power / 2;
+		}
+		
+		involved[attacker.y][attacker.x] = true;
+		attacker.power = power;
+		attacker.time = turn;
+		
+	}
+	
+	static void broke() {
+		for(int y=1; y<=n; y++) {
+			for(int x=1; x<=m; x++) {
+				if(board[y][x] == BROKEN)
+					continue;
+				if(board[y][x].power <= 0)
+					board[y][x] = BROKEN;
+			}
+		}
+	}
+	
+	static void simulate() {
+		init();
+//		if(turrets.size() == 1) { // 부서지지 않은 포탑이 1개라면 그 즉시 중지 
+//			System.out.println(target.power);
+//			System.exit(0);
+//		}
+		if(attacker == target) {
+			System.out.println(target.power);
+			System.exit(0);
+		}
+		
+		
+//		System.out.println("attacker: "+ attacker);
+//		System.out.println("target: " + target);
+		
+		
+		// 공격 
+		attack();
+		
+		// 포탑 부서짐
+		broke();
+		
+		// 포탑 정비 
+		for(int y = 1; y<=n; y++) {
+			for(int x=1; x<=m; x++) {
+				if(board[y][x] == BROKEN || involved[y][x])
+					continue;
+				
+				board[y][x].power++;
+			}
+		}
+//		printBoard(board);
+		
+	}
+	static int turn;
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        arr = new int[N][M];
-        cntArr = new int[N][M];  // 공격 턴 카운트 배열
-
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        board = new Turret[n+1][m+1];
+        involved = new boolean[n+1][m+1]; // 포탑이 공격에 연관되었는지를 판단
+        for(int y=1; y<=n; y++) {
+        	st = new StringTokenizer(br.readLine());
+        	for(int x=1; x<=m; x++) {
+        		int power = Integer.parseInt(st.nextToken());
+        		if(power != 0)
+        			board[y][x] = new Turret(power, 0, y,x);
+        		else
+        			board[y][x] = BROKEN;
+        	}
         }
-
-        for (turn = 1; turn <= K; turn++) {
-            relative = new boolean[N][M]; // 관련 없는 포탑 체크 배열
-            // 공격자 선정
-            Point attacker = findAttacker();
-            relative[attacker.x][attacker.y] = true;
-            // 공격대상 선정
-            Point strongest = strongestTower();
-            if(strongest.x == -1) break;
-            relative[strongest.x][strongest.y] = true;
-            // 레이저 공격
-            List<Point> result = laiserAttack(attacker, strongest);
-            if (result != null) {
-                // System.out.print("laiser path : ");
-                // for (Point p : result) {
-                //     System.out.print(p.x + " " + p.y + " ->");
-                // }
-                // System.out.println();
-                laserBomb(result, attacker, strongest);
-            } else { // 레이저 불가시 포탄 공격
-                bomb(attacker, strongest);
-            }
-            // printArr(arr);
-            repair(); // 공격과 무관한 포탄 정비 +1
-            addAttackTurn(attacker); // 턴 증가
+        
+        for(turn=1;turn<=k;turn++) {
+//        	System.out.println("-----");
+//        	System.out.println("turn: "+turn);
+        	simulate();
+        	
         }
-
-        getResult();
-    }
-
-    public static Point findAttacker() {
-        int power = 10000;
-        int bx = -1, by = -1;
-        for (int y = M - 1; y >= 0; y--) {
-            for (int x = N - 1; x >= 0; x--) {
-                if (arr[x][y] == 0) continue;
-                if (power >= arr[x][y]) {
-                    if (power == arr[x][y]) {
-                        if (cntArr[bx][by] < cntArr[x][y]) continue;
-                        else if (bx + by > x + y) continue;
-                        else if (by > y) continue;
-                    }
-                    power = arr[x][y];
-                    bx = x;
-                    by = y;
-                }
-            }
-        }
-        arr[bx][by] += N + M;
-        // System.out.println("attacker : " + bx + " " + by);
-        return new Point(bx, by);
-    }
-
-    /*
-     * 1. 공격력 가장 높음
-     * 2. 공격한지 오래된 포탑
-     * 3. 행과 열이 가장 작음
-     * 4. 열 값이 가장 작음
-     */
-    public static Point strongestTower() {
-        int power = 0;
-        int bx = -1, by = -1;
-        for (int y = 0; y < M; y++) {
-            for (int x = 0; x < N; x++) {
-                if (arr[x][y] == 0 || relative[x][y]) continue;
-                if (power <= arr[x][y]) {
-                    if (power == arr[x][y]) {
-                        if (cntArr[bx][by] > cntArr[x][y]) continue;
-                        else if (bx + by < x + y) continue;
-                        else if (by < y) continue;
-                    }
-                    power = arr[x][y];
-                    bx = x;
-                    by = y;
-                }
-            }
-        }
-        // System.out.println("victim : " + bx + " " + by);
-        return new Point(bx, by);
-    }
-
-    public static List<Point> laiserAttack(Point start, Point victim) {
-        int[][] pathArr = new int[N][M];
-        for (int i = 0; i < N; i++) Arrays.fill(pathArr[i], 100000);
-        pathArr[start.x][start.y] = 0;
-        int[] dx = {0, 1, 0, -1}; // 우하좌상
-        int[] dy = {1, 0, -1, 0};
-        Queue<Point> queue = new ArrayDeque<>();
-        queue.add(start);
-
-        while (!queue.isEmpty()) {
-            Point cur = queue.poll();
-            if (cur.x == victim.x && cur.y == victim.y) break;
-            for (int d = 0; d < 4; d++) {
-                int nx = (N + cur.x + dx[d]) % N;
-                int ny = (M + cur.y + dy[d]) % M;
-                if (arr[nx][ny] == 0) continue;
-                if (pathArr[nx][ny] < pathArr[cur.x][cur.y] + 1) continue;
-                pathArr[nx][ny] = pathArr[cur.x][cur.y] + 1;
-                queue.add(new Point(nx, ny));
-            }
-        }
-        return findPath(victim, pathArr);
-    }
-
-    public static List<Point> findPath(Point start, int[][] path) {
-        int[] dx = {0, 1, 0, -1}; // 우하좌상
-        int[] dy = {1, 0, -1, 0};
-        List<Point> list = new ArrayList<>();
-        int curX = start.x;
-        int curY = start.y;
-        int cur = path[start.x][start.y];
-
-        for (int d = 3; d >= 0; d--) {
-            int nx = (N + curX + dx[d]) % N;
-            int ny = (M + curY + dy[d]) % M;
-            if (path[nx][ny] == cur - 1) {
-                curX = nx;
-                curY = ny;
-                list.add(new Point(curX, curY));
-                cur--;
-                if (cur == 0) return list;
-                d = 4;
-            }
-        }
-        return null;
-    }
-
-
-    public static void laserBomb(List<Point> path, Point attack, Point victim) {
-        int power = arr[attack.x][attack.y];
-        arr[victim.x][victim.y] -= power;
-        if (arr[victim.x][victim.y] < 0) arr[victim.x][victim.y] = 0;
-        for (Point target : path) {
-            relative[target.x][target.y] = true;
-            if (target.x == attack.x && target.y == attack.y) continue;
-            else arr[target.x][target.y] -= power / 2;
-            if (arr[target.x][target.y] < 0) arr[target.x][target.y] = 0;
-        }
-    }
-
-    public static void bomb(Point attacker, Point victim) {
-        arr[victim.x][victim.y] -= arr[attacker.x][attacker.y];
-        if (arr[victim.x][victim.y] < 0) arr[victim.x][victim.y] = 0;
-        int[] dx = {0, 1, 0, -1, 1, 1, -1, -1}; // 우하좌상
-        int[] dy = {1, 0, -1, 0, 1, -1, 1, -1};
-        for (int i = 0; i < 8; i++) {
-            int nx = (N + victim.x + dx[i]) % N;
-            int ny = (M + victim.y + dy[i]) % M;
-            if (arr[nx][ny] == 0) continue;
-            if (nx == attacker.x && ny == attacker.y) continue;
-            arr[nx][ny] -= arr[attacker.x][attacker.y] / 2;
-            if (arr[nx][ny] < 0) arr[nx][ny] = 0;
-            relative[nx][ny] = true;
-        }
-
-    }
-
-
-    public static void repair() {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (relative[i][j] || arr[i][j] == 0) continue;
-                arr[i][j]++;
-            }
-        }
-    }
-
-    // 공격자를 제외한 나머지는 턴 증가. 공격자는 턴 초기화
-    public static void addAttackTurn(Point attack) {
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (arr[i][j] == 0) continue;
-                if (attack.x == i && attack.y == j) cntArr[i][j] = 0;
-                else cntArr[i][j] = turn;
-            }
-        }
-    }
-
-    public static void getResult() {
-        int max = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                max = Math.max(max, arr[i][j]);
-            }
-        }
-        System.out.println(max);
-    }
-
-    public static void printArr(int[][] arr) {
-        System.out.println();
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                System.out.print(arr[i][j] + "\t");
-            }
-            System.out.println();
-        }
-    }
-
-    static class Point {
-        int x, y;
-
-        public Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
+        
+        init();
+        System.out.println(target.power);
     }
 }
