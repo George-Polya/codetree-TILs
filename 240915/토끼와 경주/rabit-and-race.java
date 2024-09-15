3564aa40da688199a6f9e8d15d101cf9719e82b3
Kim Hee Su <wlskaka4@gmail.com>
2024-09-15 16:03:45 +0900

    Title: 토끼와 경주, Time: 621ms, Memory: 23MB, Status: Passed - CodeTree

--- 240915/토끼와 경주/rabit-and-race.java
+++ 240915/토끼와 경주/rabit-and-race.java
@@ -184,7 +184,7 @@ public class Main {
 	}
 	
 	public static void main(String[] args) throws IOException{
-		System.setIn(new FileInputStream("./input.txt"));
+//		System.setIn(new FileInputStream("./input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		q = Integer.parseInt(br.readLine());
 		
