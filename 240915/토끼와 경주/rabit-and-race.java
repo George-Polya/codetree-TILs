bc00585edb8b59da67202897dfdfc4fb4116dcf2
Kim Hee Su <wlskaka4@gmail.com>
2024-09-15 16:00:17 +0900

    Title: 토끼와 경주, Time: 995ms, Memory: 37MB, Status: Wrong Answer - CodeTree

--- 240915/토끼와 경주/rabit-and-race.java
+++ 240915/토끼와 경주/rabit-and-race.java
@@ -2,230 +2,206 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static class Rabbit {
-        int pid;
-        int d;
-        int row, col;
-        int totalJumps;
-        int score;
-        int idx;
-        boolean selectedInCurrentRace;
+	static int q,n,m,p;
+	static StringTokenizer st;
+	static class Rabbit{
+		int jumpCnt, y,x,pid, d;
+		public Rabbit(int jumpCnt, int y,int x,int pid, int d) {
+			this.jumpCnt = jumpCnt;
+			this.y = y;
+			this.x = x;
+			this.pid = pid;
+			this.d = d;
+		}
+		
+		public String toString() {
+			return String.format(" pid : %d |(%d,%d)| jumpCnt: %d| d: %d", pid, y,x,jumpCnt,d);
+		}
+	}
+	
+	static int scores[];
+	static ArrayList<Rabbit> rabbits;
+	static Map<Integer, Integer> pid2Idx;
+	static void init() {
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		p = Integer.parseInt(st.nextToken());
+		
+		pq1 = new PriorityQueue<>((a,b)->{
+			if(a.jumpCnt != b.jumpCnt)
+				return a.jumpCnt - b.jumpCnt;
+			if((a.y + a.x) != (b.y + b.x))
+				return (a.y + a.x) - (b.y+b.x);
+			if(a.y != b.y)
+				return a.y - b.y;
+			if(a.x != b.x)
+				return a.x - b.x;
+			
+			return a.pid - b.pid;
+		});
+		
+		
+		rabbits = new ArrayList<>();
+		pid2Idx = new HashMap<>();
+		scores = new int[p];
+		
+		for(int idx = 0; idx < p; idx++) {
+			int pid = Integer.parseInt(st.nextToken());
+			int d = Integer.parseInt(st.nextToken());
+			Rabbit rabbit = new Rabbit(0,1,1,pid,d);
+			rabbits.add(rabbit);
+			pq1.add(rabbit);
+			pid2Idx.put(pid, idx);
+		}
+		
+	}
 
-        public Rabbit(int pid, int d, int idx) {
-            this.pid = pid;
-            this.d = d;
-            this.row = 1;
-            this.col = 1;
-            this.totalJumps = 0;
-            this.score = 0;
-            this.idx = idx;
-            this.selectedInCurrentRace = false;
-        }
-    }
+	static PriorityQueue<Rabbit> pq1;
+	static PriorityQueue<Rabbit> pq2;
+	
+	static class Pair implements Comparable<Pair>{
+		int y,x;
+		public Pair(int y,int x) {
+			this.y = y;
+			this.x = x;
+		}
+		
+		public String toString() {
+			return y+" "+x;
+		}
+		
+		public int compareTo(Pair o) {
+			if((y + x) != (o.y + o.x))
+				return (o.y + o.x) - (y+x);
+			if(y != o.y)
+				return o.y - y;
+			return o.x - x;
+		}
+		
+	}
+	
+	static Pair NO_POS = new Pair(-1,-1);
+	
+	static int dy[] = {-1,1,0,0};
+	static int dx[] = {0,0,-1,1};
+	static boolean OOB(int y,int x) {
+		return y<=0 || y>n || x<=0 || x>m;
+	}
+	
+	static Pair findBestPos(Rabbit rabbit) {
+		int y = rabbit.y;
+		int x = rabbit.x;
+		int d = rabbit.d;
+		
+		Pair ret = NO_POS;
+		
+		for(int dir = 0; dir < 4; dir++) {
+//			System.out.println("=====");
+			int ny = y + dy[dir] * (d % (2 * (n-1)));
+			int nx = x + dx[dir] * (d % (2 * (m-1)));
+//			System.out.printf("before %d %d\n", ny,nx);
+			while(OOB(ny,nx)) {
+				if(ny <= 0)
+					ny = 2 - ny;
+				else if(ny > n)
+					ny = 2 * n - ny;
+				
+				if(nx <= 0)
+					nx = 2 - nx;
+				else if(nx > m)
+					nx = 2 * m - nx;
+			}
+//			System.out.printf("after %d %d\n",ny,nx);
+			
+			Pair pair = new Pair(ny,nx);
+			if(ret.compareTo(pair) > 0) {
+				ret = pair;
+			}
+			
+		}
+		return ret;
+	}
+	
+	static int total;
+	
+	static void race() {
+		int k = Integer.parseInt(st.nextToken());
+		int s = Integer.parseInt(st.nextToken());
+		
+		
+		pq2 = new PriorityQueue<>((a,b)->{
+			if((a.y + a.x) != (b.y + b.x) )
+				return (b.y + b.x) - (a.y + a.x);
+			if(a.y != b.y)
+				return b.y - a.y;
+			if(a.x != b.x)
+				return b.x - a.x;
+			return b.pid - a.pid;
+		});
+		
+		
+		for(int turn =1; turn <=k ;turn++) {
+//			System.out.println("-----");
+//			System.out.println("turn: "+turn);
+//			System.out.println("pq1: "+pq1);
+			
+			Rabbit best = pq1.poll();
+//			System.out.println("bestRabbit: "+best);
+			
+			Pair bestPos = findBestPos(best);
+//			System.out.println("bestPos: "+bestPos);
+			best.y = bestPos.y;
+			best.x = bestPos.x;
+			best.jumpCnt++;
+			pq1.add(best);
+			pq2.add(best);
+			
+			int idx = pid2Idx.get(best.pid);
+			scores[idx] -= (bestPos.y + bestPos.x);
+			total += (bestPos.y + bestPos.x);
+		}
+		
 
-    static int N, M, P;
-    static HashMap<Integer, Rabbit> rabbitsMap = new HashMap<>();
-    static List<Rabbit> rabbitsList = new ArrayList<>();
-    static int[] dr = {-1, 1, 0, 0}; // 상하좌우
-    static int[] dc = {0, 0, -1, 1}; // 상하좌우
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        int Q = Integer.parseInt(br.readLine());
-
-        for (int cmdCount = 0; cmdCount < Q; cmdCount++) {
-            st = new StringTokenizer(br.readLine());
-            int cmd = Integer.parseInt(st.nextToken());
-
-            if (cmd == 100) { // 경주 시작 준비
-                N = Integer.parseInt(st.nextToken());
-                M = Integer.parseInt(st.nextToken());
-                P = Integer.parseInt(st.nextToken());
-                for (int i = 0; i < P; i++) {
-                    int pid = Integer.parseInt(st.nextToken());
-                    int d = Integer.parseInt(st.nextToken());
-                    Rabbit rabbit = new Rabbit(pid, d, i);
-                    rabbitsMap.put(pid, rabbit);
-                    rabbitsList.add(rabbit);
-                }
-            } else if (cmd == 200) { // 경주 진행
-                int K = Integer.parseInt(st.nextToken());
-                int S = Integer.parseInt(st.nextToken());
-                race(K, S);
-            } else if (cmd == 300) { // 이동거리 변경
-                int pid_t = Integer.parseInt(st.nextToken());
-                int L = Integer.parseInt(st.nextToken());
-                Rabbit rabbit = rabbitsMap.get(pid_t);
-                if (rabbit != null) {
-                    rabbit.d *= L;
-                }
-            } else if (cmd == 400) { // 최고의 토끼 선정
-                int maxScore = 0;
-                for (Rabbit rabbit : rabbitsList) {
-                    if (rabbit.score > maxScore) {
-                        maxScore = rabbit.score;
-                    }
-                }
-                System.out.println(maxScore);
-            }
-        }
-    }
-
-    static void race(int K, int S) {
-        // 현재 경주에서 선택된 토끼 초기화
-        for (Rabbit rabbit : rabbitsList) {
-            rabbit.selectedInCurrentRace = false;
-        }
-
-        for (int k = 0; k < K; k++) {
-            Rabbit selectedRabbit = null;
-            for (Rabbit rabbit : rabbitsList) {
-                if (selectedRabbit == null || compareRabbitsForSelection(rabbit, selectedRabbit) < 0) {
-                    selectedRabbit = rabbit;
-                }
-            }
-
-            // 토끼 이동
-            moveRabbit(selectedRabbit);
-
-            selectedRabbit.totalJumps++;
-            selectedRabbit.selectedInCurrentRace = true;
-
-            int scoreToAdd = selectedRabbit.row + selectedRabbit.col;
-
-            // 다른 토끼들 점수 증가
-            for (Rabbit rabbit : rabbitsList) {
-                if (rabbit.pid != selectedRabbit.pid) {
-                    rabbit.score += scoreToAdd;
-                }
-            }
-        }
-
-        // 경주 후 추가 점수 부여
-        Rabbit bestRabbit = null;
-        for (Rabbit rabbit : rabbitsList) {
-            if (rabbit.selectedInCurrentRace) {
-                if (bestRabbit == null || compareRabbitsForScore(rabbit, bestRabbit) > 0) {
-                    bestRabbit = rabbit;
-                }
-            }
-        }
-
-        if (bestRabbit != null) {
-            bestRabbit.score += S;
-        }
-    }
-
-    static void moveRabbit(Rabbit rabbit) {
-        int maxRowColSum = -1;
-        int maxRow = -1;
-        int maxCol = -1;
-        int selectedRow = -1;
-        int selectedCol = -1;
-
-        for (int dir = 0; dir < 4; dir++) {
-            int d = rabbit.d;
-            int r = rabbit.row;
-            int c = rabbit.col;
-            int drc = dr[dir];
-            int dcc = dc[dir];
-
-            int steps = 0;
-            while (steps < d) {
-                int nr = r + drc;
-                int nc = c + dcc;
-                if (nr < 1 || nr > N || nc < 1 || nc > M) {
-                    // 방향 전환 및 한 칸 이동
-                    drc = -drc;
-                    dcc = -dcc;
-                    nr = r + drc;
-                    nc = c + dcc;
-                    // 방향 전환 후에도 격자 밖인 경우 이동 중단
-                    if (nr < 1 || nr > N || nc < 1 || nc > M) {
-                        break;
-                    }
-                    r = nr;
-                    c = nc;
-                    steps++;
-                    break;
-                } else {
-                    r = nr;
-                    c = nc;
-                    steps++;
-                }
-            }
-
-            while (steps < d) {
-                int nr = r + drc;
-                int nc = c + dcc;
-                if (nr < 1 || nr > N || nc < 1 || nc > M) {
-                    // 방향 전환 및 한 칸 이동
-                    drc = -drc;
-                    dcc = -dcc;
-                    nr = r + drc;
-                    nc = c + dcc;
-                    // 방향 전환 후에도 격자 밖인 경우 이동 중단
-                    if (nr < 1 || nr > N || nc < 1 || nc > M) {
-                        break;
-                    }
-                    r = nr;
-                    c = nc;
-                    steps++;
-                } else {
-                    r = nr;
-                    c = nc;
-                    steps++;
-                }
-            }
-
-            int rowColSum = r + c;
-            if (rowColSum > maxRowColSum ||
-                    (rowColSum == maxRowColSum && r > maxRow) ||
-                    (rowColSum == maxRowColSum && r == maxRow && c > maxCol)) {
-                maxRowColSum = rowColSum;
-                maxRow = r;
-                maxCol = c;
-                selectedRow = r;
-                selectedCol = c;
-            }
-        }
-
-        // 위치 업데이트
-        rabbit.row = selectedRow;
-        rabbit.col = selectedCol;
-    }
-
-    static int compareRabbitsForSelection(Rabbit a, Rabbit b) {
-        if (a.totalJumps != b.totalJumps) {
-            return Integer.compare(a.totalJumps, b.totalJumps);
-        }
-        int aPosSum = a.row + a.col;
-        int bPosSum = b.row + b.col;
-        if (aPosSum != bPosSum) {
-            return Integer.compare(aPosSum, bPosSum);
-        }
-        if (a.row != b.row) {
-            return Integer.compare(a.row, b.row);
-        }
-        if (a.col != b.col) {
-            return Integer.compare(a.col, b.col);
-        }
-        return Integer.compare(a.pid, b.pid);
-    }
-
-    static int compareRabbitsForScore(Rabbit a, Rabbit b) {
-        int aPosSum = a.row + a.col;
-        int bPosSum = b.row + b.col;
-        if (aPosSum != bPosSum) {
-            return Integer.compare(aPosSum, bPosSum);
-        }
-        if (a.row != b.row) {
-            return Integer.compare(a.row, b.row);
-        }
-        if (a.col != b.col) {
-            return Integer.compare(a.col, b.col);
-        }
-        return Integer.compare(a.pid, b.pid);
-    }
+//		System.out.println("peek: "+pq2.peek());
+		pq2.addAll(pq1);
+		int pid = pq2.peek().pid;
+		int idx = pid2Idx.get(pid);
+		scores[idx] += s;
+	}
+	
+	static void changeDistance() {
+		int pid = Integer.parseInt(st.nextToken());
+		int l = Integer.parseInt(st.nextToken());
+		
+		int idx = pid2Idx.get(pid);
+		Rabbit rabbit = rabbits.get(idx);
+		rabbit.d *= l;
+	}
+	static int bestScore() {
+		int ans = Integer.MIN_VALUE;
+		for(int i = 0; i < p; i++) {
+			ans = Math.max(ans, scores[i]);
+		}
+		return ans + total;
+	}
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		q = Integer.parseInt(br.readLine());
+		
+		for(int t = 1; t <= q; t++) {
+			st = new StringTokenizer(br.readLine());
+			int cmd = Integer.parseInt(st.nextToken());
+			
+			if(cmd == 100) {
+				init();
+			}else if(cmd == 200) {
+				race();
+			}else if(cmd == 300) {
+				changeDistance();
+			}else if(cmd == 400) {
+				System.out.println(bestScore());
+			}
+		}
+	}
 }
